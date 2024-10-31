fa5d4ed903f325649c6560dafdea72d4b03cd355
Valentin Vucea <valentinvucea@gmail.com>
2024-10-31 17:20:22 -0400

    Refactoring - restructure quiz module

---
+++ app/(modules)/quiz/[id]/exam/page.tsx
@@ -0,0 +1,9 @@
+const QuizGeneratePage = () => {
+    return (
+        <div>
+            <h2 className="text-w-2xl">Exam</h2>
+        </div>
+    );
+};
+
+export default QuizGeneratePage;
---
+++ app/(modules)/quiz/generate/page.tsx
@@ -0,0 +1,5 @@
+const QuizGeneratePage = () => {
+    return <h1 className="text-w-3xl">Generate Quiz</h1>;
+};
+
+export default QuizGeneratePage;
