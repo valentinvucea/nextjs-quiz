d091048ee33d5d1faea903d8d550c505e5ec5192
Valentin Vucea <valentin@itworksdc.com>
2024-11-04 20:00:31 -0500

    Small change in menu

--- app/lib/menu.ts
+++ app/lib/menu.ts
@@ -4,7 +4,7 @@ export const Menu = [
         title: "Profile",
         submenu: [
             { title: "Info", url: "/profile/info" },
-            { title: "Results", url: "/profile/results" },
+            { title: "Results", url: "/profile" },
         ],
     },
     {
